f9988426866deb4b371cfec132b9e2ca5a9ba21b
KnownUsername <joaoftrodrigues@gmail.com>
2022-01-09 02:59:36 +0000

    Added operators' dict; Changed and adapted arguments to args; Added exceptions for previous changes; Added base evaluators

--- commands_eval.py
+++ commands_eval.py
@@ -5,9 +5,39 @@ class CommandsEval:
 
     loaded_tables = {}
 
+    operators = {
+        "LOAD": lambda args, filename: CommandsEval._load(args),
+        "DISCARD": lambda args: CommandsEval._discard(args),
+        "SAVE": lambda args, filename: CommandsEval._save(args),
+        "SHOW": lambda args: CommandsEval._show(args),
+        "CREATE": lambda args: args,
+        "SELECT": lambda args: args
+    }
+
+
     @staticmethod
-    def load(table_name, filename):
+    def _load(args):
         """ Loads a table - Loads csv file to memory """
+
+        # Check if values' fields exists
+        try:
+            table_name = args['table']
+        except KeyError:
+            raise KeyError('Table value was not found')
+
+        try:
+            filename = args['file']
+        except KeyError:
+            raise KeyError(f' Filename was not found')
+
+
+        # Check if there is only 1 value that is a string
+        if type(table_name) is not str:
+            raise TypeError("Table has invalid type")
+        if type(filename) is not str:
+            raise TypeError("Filename has invalid type")
+
+
 
         # Check if file's extension is csv
         if not CommandsEval.is_csv(filename):
@@ -31,8 +61,18 @@ class CommandsEval:
         return True
 
     @staticmethod
-    def discard(table_name):
+    def _discard(args):
         """ Discards a table - removes from memory """
+
+        # Check if field exists
+        try:
+            table_name = args['table']
+        except KeyError:
+            raise KeyError('Table value was not found')
+
+        # Check if there is only 1 value that is a string
+        if type(table_name) is not str:
+            raise TypeError("Table has invalid type")
 
         # Table must be loaded - on tables' dictionary
         if table_name in CommandsEval.loaded_tables:
@@ -43,8 +83,18 @@ class CommandsEval:
             raise Exception('Table is not loaded')
 
     @staticmethod
-    def show(table_name):
+    def _show(args):
         """ Presents table on console"""
+
+        # Check if field exists
+        try:
+            table_name = args['table']
+        except KeyError:
+            raise KeyError('Table value was not found')
+
+        # Check if there is only 1 value that is a string
+        if type(table_name) is not str:
+            raise TypeError("Table has invalid type")
 
         # Table must be loaded - on tables' dictionary
         if table_name in CommandsEval.loaded_tables:
@@ -52,8 +102,29 @@ class CommandsEval:
         return True
 
     @staticmethod
-    def save(table_name, filename):
+    def _save(args):
         """ Saves table on a csv file"""
+
+        # Check if fields exists
+        try:
+            table_name = args['table']
+        except KeyError:
+            raise KeyError(f'Table value was not found')
+
+        try:
+            filename = args['file']
+        except KeyError:
+            raise KeyError(f' Filename was not found')
+
+
+        # Check if there is only 1 value, that is a string
+        if type(table_name) is not str:
+            raise TypeError("Table has invalid type")
+        if type(filename) is not str:
+            raise TypeError("Filename has invalid type")
+
+
+
 
         # Check if file is loaded
         if table_name not in CommandsEval.loaded_tables:
@@ -70,7 +141,7 @@ class CommandsEval:
         try:
             # Store table into a csv file
             CommandsEval.loaded_tables[table_name].to_csv(filename)
-        except:
+        except Exception:
             raise Exception
         finally:
             print(f"Table {table_name} was successfully saved as {filename}")
@@ -83,5 +154,34 @@ class CommandsEval:
     @staticmethod
     def is_csv(filename):
         """ Checks if file extension is csv """
-
         return True if filename[-4:] == '.csv' else False
+
+    @staticmethod
+    def evaluate(ast):
+        if type(ast) in (bool, float, str):
+            return ast
+        if type(ast) is dict:
+            return CommandsEval._eval_operator(ast)
+        if type(ast) is list:
+            ans = None
+            for a in ast:
+                ans = CommandsEval.evaluate(a)
+            return ans
+
+        raise Exception("Unknown AST type")
+
+    @staticmethod
+    def _eval_operator(ast):
+
+        if 'op' in ast:
+            op = ast["op"]
+
+            args = [arg for arg in ast['args']]
+
+            if op in CommandsEval.operators:
+                func = CommandsEval.operators[op]
+                return func(args)
+            else:
+                raise Exception(f"Unknown operator {op}")
+
+        raise Exception('Undefined AST')
