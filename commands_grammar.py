abfa33cc7da74e59a9208e7e0d27359dc8201a55
KnownUsername <joaoftrodrigues@gmail.com>
2022-01-09 03:10:03 +0000

    Added p_save() #16

--- commands_grammar.py
+++ commands_grammar.py
@@ -9,3 +9,7 @@ class CommandsGrammar:
     def p_discard(self, p):
         """ q_discard: DISCARD TABLE var """
         p[0] = {"op": p[1], "args": {"table": p[3]}}
+
+    def p_save(self, p):
+        """ q_save: SAVE TABLE var AS file """
+        p[0] = {"op": p[1], "args": {"table": p[3], "file": p[5]}}
