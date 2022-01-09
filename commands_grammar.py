334c8b03d4972502217064856b83668f3721c105
KnownUsername <joaoftrodrigues@gmail.com>
2022-01-09 03:06:25 +0000

    Added p_discard() #15

--- commands_grammar.py
+++ commands_grammar.py
@@ -6,3 +6,6 @@ class CommandsGrammar:
         """ q_load : LOAD TABLE var FROM file """
         p[0] = {"op": p[1], "args": {"table": p[3], "file": p[5]}}
 
+    def p_discard(self, p):
+        """ q_discard: DISCARD TABLE var """
+        p[0] = {"op": p[1], "args": {"table": p[3]}}
