9e8b7e7ca94419f69529917d747bb9d86ae3a8a4
KnownUsername <joaoftrodrigues@gmail.com>
2022-01-16 23:31:09 +0000

    Allows using commands from text file and continue on executing commands con command line

--- tql.py
+++ tql.py
@@ -1,6 +1,7 @@
 from commands_grammar import CommandsGrammar
 from pprint import PrettyPrinter
 from commands_eval import CommandsEval
+import sys
 
 cg = CommandsGrammar()
 cg.build()
@@ -8,7 +9,20 @@ cg.build()
 
 pp = PrettyPrinter()
 
+# Check if has filename
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
 
+    with open(filename, "r") as f:
+        content = f.read()
+        try:
+            ans = cg.parse(content)
+            if ans is not None:
+                print(ans)
+        except Exception as exception:
+            print(exception)
+
+# For commands input on console
 for expr in iter(lambda: input(">> "), ""):
     try:
         ans = cg.parse(expr)
